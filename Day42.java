ea729acff430419f2768b46aa2a547c6ddd0255f
Dayananda  K S <dayanandaks045@gmail.com>
2026-03-15 22:10:13 +0530

    Implement LinkedList with insert and display methods

---
+++ Day42.java
@@ -0,0 +1,35 @@
+public class LinkedList {
+    Node head;
+    class Node {
+        int data;
+        Node next;
+    }
+    public void insert(int data) {
+        Node node = new Node();
+        node.data = data;
+        node.next = null;
+        if (head == null) {
+            head = node;
+        } else {
+            Node temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = node;
+        }
+    }
+    public void display() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.insert(10);
+        list.insert(20);
+        list.display();
+    }
+}
