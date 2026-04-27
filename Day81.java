0814ab59be6b201574ab7a2e387525d013a9d282
Dayananda  K S <dayanandaks045@gmail.com>
2026-04-27 21:53:12 +0530

    Improve comments in backtrack method
    Refactor backtrack method comments for clarity.

--- Day81.java
+++ Day81.java
@@ -7,7 +7,7 @@ class Solution {
 
     private void backtrack(int[] nums, List<Integer> current, List<List<Integer>> result) {
         
-        // If permutation is complete
+        
         if (current.size() == nums.length) {
             result.add(new ArrayList<>(current));
             return;
@@ -15,18 +15,17 @@ class Solution {
 
         for (int num : nums) {
             
-            // Skip if number already used
+        
             if (current.contains(num)) {
                 continue;
             }
 
-            // Choose
             current.add(num);
 
-            // Explore
+    
             backtrack(nums, current, result);
 
-            // Backtrack
+        
             current.remove(current.size() - 1);
         }
     }
